f853c49c093619a31be0b0da72e5968c99964063
Katsiaryna Shlemena <96011719+Kate4ka007@users.noreply.github.com>
2023-08-01 11:53:11 +0000

    feat: add files

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.listen(5000).then(()=> {
+    console.log("successfully stared on port 5000");
+  }).catch((error)=>{
+    console.log(error)
+  })
 }
 bootstrap();
